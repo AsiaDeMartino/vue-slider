d64cf07e10fbf6a3fac8cc4f8d9e4e52cc62ee76
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-25 15:50:01 +0100

    bonus 2

--- myscript.js
+++ myscript.js
@@ -35,6 +35,7 @@ const container = new Vue (
                 ],
 
                 indice: 0,
+                clock: undefined,
                 
         },
 
@@ -59,171 +60,10 @@ const container = new Vue (
                 this.indice = j;
             },
              
-        }       
+        },
+        
+        mounted(){
+            setInterval( this.next ,3000);
+        },
 });
-
-
-
-//definisco le variabili
-// const ITEMWRAPPER = document.querySelector(".item-wrapper");
-// const SLIDEWRAPPER = document.querySelector(".slide-wrapper");
-
-
-
-//ciclo for per inserire img slider
-
-// for (let i = 0; i < immaginiSlider.length; i++) {
-//     const ITEM = document.createElement("div");
-//     ITEMWRAPPER.append(ITEM);
-//     ITEM.classList.add("item-" + (i) , "item" );
-
-//     let immagini = document.createElement("img");
-//     immagini.src = immaginiSlider[i].immagine;
-//     ITEM.append(immagini);
-// }
-
-// console.log(ITEMWRAPPER)
-
-// let i = 0;
-
-// immaginiSlider.forEach( () => {
-//     const ITEM = document.createElement("div");
-//     ITEMWRAPPER.append(ITEM);
-//     ITEM.classList.add("item-" + (i) , "item" );
-
-//     let immagini = document.createElement("img");
-//     immagini.src = immaginiSlider[i].immagine;
-//     ITEM.append(immagini);
-//     i++;
-// } );
-
-
-
-//ciclo for per inserire img grande
-// for (let i = 0; i < immaginiSlider.length; i++) {
-//     const ITEM = document.createElement("div");
-//     SLIDEWRAPPER.append(ITEM);
-//     ITEM.classList.add("slide-" + (i) , "slide" );
-
-//     let immagini = document.createElement("img");
-//     immagini.src = immaginiSlider[i].immagine;
-//     ITEM.append(immagini);
-
-//     const TEXTWRAPPER = document.createElement("div");
-//     ITEM.append(TEXTWRAPPER);
-//     TEXTWRAPPER.classList.add("testo");
-    
-//     let titolo = document.createElement("h1");
-//     TEXTWRAPPER.append(titolo);
-//     titolo.innerHTML += immaginiSlider[i].titolo
-
-//     let testo = document.createElement("h4");
-//     TEXTWRAPPER.append(testo);
-//     testo.innerHTML += immaginiSlider[i].descrizione;
-// }
-
-// console.log(SLIDEWRAPPER);
-
-//reset variabile indice
-// i = 0;
-
-// immaginiSlider.forEach( () => {
-
-//     const ITEM = document.createElement("div");
-//     SLIDEWRAPPER.append(ITEM);
-//     ITEM.classList.add("slide-" + (i) , "slide" );
-
-//     let immagini = document.createElement("img");
-//     immagini.src = immaginiSlider[i].immagine;
-//     ITEM.append(immagini);
-
-//     const TEXTWRAPPER = document.createElement("div");
-//     ITEM.append(TEXTWRAPPER);
-//     TEXTWRAPPER.classList.add("testo");
-    
-//     let titolo = document.createElement("h1");
-//     TEXTWRAPPER.append(titolo);
-//     titolo.innerHTML += immaginiSlider[i].titolo
-
-//     let testo = document.createElement("h4");
-//     TEXTWRAPPER.append(testo);
-//     testo.innerHTML += immaginiSlider[i].descrizione;
-
-//     i++;
-
-// } );
-
-
-//reset variabile indice
-// i = 0;
-
-
-//dare active alle prime immagini
-// let activeSlide = document.querySelector(`.slide-${i}`);
-// activeSlide.classList.add("active");
-
-// let activeItem = document.querySelector(`.item-${i}`);
-// activeItem.classList.add("active");
-
-
-//rendere funzionante lo slider
-
-    //aggiungere costanti per i pulsanti 
-// const NEXT = document.getElementById("next");
-// console.log(NEXT);
-
-// const PREV = document.getElementById("prev");
-// console.log(PREV);
-
-    //Pulsate prev
-// PREV.addEventListener("click", function(){
-//     let activeItem = document.querySelector(`.item-${i}`);
-//     let activeSlide = document.querySelector(`.slide-${i}`);
-//     activeItem.classList.remove("active");
-//     activeSlide.classList.remove("active");
-
-//     if (i > 0) {
-//         i--;
-//         let activeItem = document.querySelector(`.item-${i}`);
-//         let activeSlide = document.querySelector(`.slide-${i}`);
-//         activeItem.classList.add("active");
-//         activeSlide.classList.add("active");
-//     } else {
-//         i = immaginiSlider.length-1;
-//         let activeItem = document.querySelector(`.item-${i}`);
-//         let activeSlide = document.querySelector(`.slide-${i}`);
-//         activeItem.classList.add("active");
-//         activeSlide.classList.add("active");
-//     }
-//     console.log(i);
-// })
-
-
-//     console.log(i)
-
-    //Pulsante next
-// NEXT.addEventListener("click", function(){
-//     let activeItem = document.querySelector(`.item-${i}`);
-//     let activeSlide = document.querySelector(`.slide-${i}`);
-//     activeItem.classList.remove("active");
-//     activeSlide.classList.remove("active");
-
-//     if (i < immaginiSlider.length-1) {
-//         i++
-//         let activeItem = document.querySelector(`.item-${i}`);
-//         let activeSlide = document.querySelector(`.slide-${i}`);
-//         activeItem.classList.add("active");
-//         activeSlide.classList.add("active");
-//     } else {
-//         i = 0
-//         let activeItem = document.querySelector(`.item-${i}`);
-//         let activeSlide = document.querySelector(`.slide-${i}`);
-//         activeItem.classList.add("active");
-//         activeSlide.classList.add("active");
-//     }
-//     console.log(i);
-// })
-
-
-
 
